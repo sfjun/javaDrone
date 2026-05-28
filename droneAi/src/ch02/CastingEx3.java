5f5582dc1bd5e5e3da835882252c60e8a5988873
theGyoo <sfjun@naver.com>
2026-05-28 15:54:54 +0900

    3rd commit castingEx3

---
+++ droneAi/src/ch02/CastingEx3.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class CastingEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float f = 9.1234567f;
+		
+		System.out.printf("f=%20.18f%n", f);
+
+	}
+
+}
